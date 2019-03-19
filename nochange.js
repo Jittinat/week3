cf8673598290629c6ffc72f718d7de4c14ab0646
Jittinat <37908367+Jittinat@users.noreply.github.com>
2019-03-19 12:48:38 +0700

    Add files via upload

---
+++ nochange.js
@@ -0,0 +1,14 @@
+// nochange.js
+//   This script illustrates using the focus event
+//   to prevent the user from changing a text field
+
+// The event handler function to compute the cost
+
+function computeCost(fr) {
+//add your code here
+
+// Compute the cost
+
+  document.getElementById("cost").value = 
+  totalCost = 3.49 * fr
+}  //* end of computeCost
